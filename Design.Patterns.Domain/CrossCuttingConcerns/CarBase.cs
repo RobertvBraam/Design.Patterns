8e102d7dcc45eab69858e7e0023a6e33ec234032
Robert van Braam <robert@omniaretail.com>
2019-08-18 20:36:37 +0200

    Setup Cross Cutting Concerns

---
+++ Design.Patterns.Domain/CrossCuttingConcerns/CarBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design.Patterns.Domain.CrossCuttingConcerns
+{
+    public class CarBase
+    {
+        public Guid Id { get; set; }
+        public CarMake CarMake { get; set; }
+    }
+
+    public enum CarMake
+    {
+        Mercedes,
+        Ford,
+        Porche,
+        Audi,
+        Peugot
+    }
+}
---
+++ Design.Patterns.Domain/CrossCuttingConcerns/CustomerBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design.Patterns.Domain.CrossCuttingConcerns
+{
+    public class CustomerBase
+    {
+        public Guid Id { get; set; }
+        string Name { get; set; }
+        public IEnumerable<CarBase> Cars { get; set; }
+    }
+}
