fe0ea69859f28cd16d8a12b2d561af13e7f53fe6
Robert van Braam <robert@omniaretail.com>
2019-08-18 20:38:02 +0200

    Setup manufacture domain

---
+++ Design.Patterns.Domain/Manufacture/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Design.Patterns.Domain.CrossCuttingConcerns;
+
+namespace Design.Patterns.Domain.Manufacture
+{
+    public class Car : CarBase
+    {
+    }
+}
---
+++ Design.Patterns.Domain/Manufacture/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design.Patterns.Domain.Manufacture
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public IEnumerable<Car> Cars { get; set; }
+    }
+}
