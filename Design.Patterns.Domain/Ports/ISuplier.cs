12b0e852679b89620e4da019195dc8a176c654c3
Robert van Braam <robert@omniaretail.com>
2019-08-18 20:38:24 +0200

    Initial though about supplier port

---
+++ Design.Patterns.Domain/Ports/ISuplier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design.Patterns.Domain.Ports
+{
+    public interface ISuplier
+    {
+        Guid Id { get; set; }
+        string Name { get; set; }
+        List<CarPart> GetCarParts();
+    }
+
+    public enum CarPart
+    {
+        Wheel,
+        Engine,
+        Chassis
+    }
+}
