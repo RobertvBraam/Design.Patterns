389ae4851491a364073d9b4e02341532411f3387
Robert van Braam <robert@omniaretail.com>
2019-08-18 20:37:31 +0200

    Setup Finance domain

---
+++ Design.Patterns.Domain/Finance/Car.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Design.Patterns.Domain.CrossCuttingConcerns;
+
+namespace Design.Patterns.Domain.Finance
+{
+    public class Car : CarBase
+    {
+    }
+}
